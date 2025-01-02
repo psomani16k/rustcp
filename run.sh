c0ef5adf1382c97257d2f1c294892c3c3bc90a86
parth <psomani16k@gmail.com>
2025-01-02 09:00:40 +0530

    no idea what it is, backing up

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 cargo build
-sudo setcap cap_net_admin=eip /home/parth/Projects/rustcp/target/debug/rustcp
-/home/parth/Projects/rustcp/target/debug/rustcp &
-pid= $!
-# sudo ip addr add 192.168.0.1/24 dev mera_tun
-# sudo ip link set up dev mera_tun
-wait pid
+stat=$?
+if [[ $stat -ne 0 ]]; then
+	exit $stat
+fi
+sudo setcap cap_net_admin=eip target/debug/rustcp
+./target/debug/rustcp &
+pid=$!
+echo $pid
+sudo ip addr add 192.168.0.1/24 dev mera_tun
+sudo ip link set up dev mera_tun
+trap "kill $pid" INT TERM
+wait $pid
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
+use etherparse::IpNumber;
+
 fn main() {
     let vnic = tun_tap::Iface::new("mera_tun", tun_tap::Mode::Tun).expect("Did not create tun");
     let mut buf = [0u8; 1600];
     loop {
         let nbytes = vnic.recv(&mut buf).unwrap();
-        eprintln!("recieved {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+        // let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        //
+        // ignoring any packet that is not a IPv4 header
+        if proto != 0x0800 {
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let protocol = p.protocol();
+                if protocol != IpNumber::TCP {
+                    println!(
+                        "Recieved a non-TCP packet of type: {:?}",
+                        protocol.keyword_str()
+                    );
+                    continue;
+                }
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(t) => {
+                        let src = t.source_port();
+                        let dest = t.destination_port();
+                        println!("{} -> {}, Size: {}", src, dest, t.window_size(),);
+                    }
+                    Err(_) => {
+                        println!("Recieved malformed TCP packet");
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Recieved some weird packet, unable to parse");
+            }
+        }
     }
 }
